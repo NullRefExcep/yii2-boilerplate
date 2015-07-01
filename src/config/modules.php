40dca95ba26e37649db783439f6a6d6c4d33115f
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-07-01 11:39:47 +0300

    add console app

--- src/config/modules.php
+++ src/config/modules.php
@@ -2,5 +2,5 @@
 
 return [
     'core' => ['class' => 'nullref\core\Module'],
-
+    'product' => ['class' => 'nullref\product\Module'],
 ];
--- src/views/site/index.php
+++ src/views/site/index.php
@@ -1,10 +1,15 @@
 <?php
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 $this->title = Yii::$app->name;
 ?>
 <div class="site-index">
     <h1>Hello world</h1>
+    <a href="<?= Url::to(['/core/admin']) ?>">Admin</a>
     <pre>
+        <?php print_r(Yii::$app->modules) ?>
+
+
     </pre>
 </div>
